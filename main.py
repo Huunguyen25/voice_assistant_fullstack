8e5282316df1cfc1b07ca962de4807fb8766c885
Huunguyen25 <huunguyen25@csu.fullerton.edu>
2024-11-25 16:50:22 -0500

    updates on settings panel

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ import threading
 # Initialize the eel application
 eel.init("www")
 
+
 # Start the server in a separate thread to ensure the server is ready before launching Chromium
 def start_server():
-    eel.start('index.html', mode=None, host='localhost', block=True, size=(940, 480))
+    eel.start("index.html", mode=None, host="localhost", block=True, size=(940, 480))
+
 
 thread = threading.Thread(target=start_server, daemon=True)
 thread.start()
 
 # Launch Chromium in app mode
-os.system('chromium --app="http://localhost:8000/index.html" --enable-features=WebAppEnableResizableWindow')
+os.system(
+    'chromium --app="http://localhost:8000/index.html" --enable-features=WebAppEnableResizableWindow'
+)
