fab14503ef9ba18dd335298012d8e3f31ede7c88
Huunguyen25 <huunguyen25@csu.fullerton.edu>
2025-02-06 19:17:26 -0500

    implements sqlite db next

--- engine/db.py
+++ engine/db.py
@@ -3,4 +3,10 @@ import sqlite3
 conn = sqlite3.connect("database.db")
 cursor = conn.cursor()
 
-query = "CREATE TABLE IF NOT EXISTS sys_command()"
+# query = "CREATE TABLE IF NOT EXISTS sys_command(id INTEGER PRIMARY KEY, name varchar(100), path varchar(1000))"
+# cursor.execute(query)
+
+query = "INSERT INTO sys_command VALUES (null, '', '')"
+cursor.execute(query)
+
+conn.commit()
